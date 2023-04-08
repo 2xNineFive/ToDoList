c9ecf610d64e4c1cf18b46113713ade19ff2ee06
Frankie Molina <officialgoldminded@gmail.com>
2023-04-08 08:45:55 -0700

    Turn off database. Could not get it to work.

--- main.js
+++ main.js
@@ -5,11 +5,38 @@ const toDoList = document.getElementById("list")
 
 const textInput = document.getElementById("text-input")
 
+
+// if (localStorage.storedList) {
+//     loadList();
+// }
+
+
 function addItem() {
     // alert(`We're working`)
     const newItem = document.createElement("p");
     newItem.innerText = textInput.value;
+    newItem.onclick = removeItem;
     toDoList.appendChild(newItem);
+    // saveList();
     textInput.value = '';
     
 }
+
+
+function removeItem() {
+    saveList();
+    toDoList.removeChild(this);
+
+}
+
+function saveList () {
+    localStorage.storedList = toDoList.innerHTML
+}
+
+function loadList () {
+    toDoList.innerHTML = localStorage.storedList;
+    for(i = 0; i < toDoList.children.length; i++) {
+    toDoList.children[i].onlclick = removeItem;
+    }
+}
+
