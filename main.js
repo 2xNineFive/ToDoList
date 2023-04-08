c16e80a9b33ef6cce49a758e7d42e49532f95697
Frankie Molina <officialgoldminded@gmail.com>
2023-04-07 19:20:43 -0700

    Add dynamic items to the to-do list

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const addBtn = document.getElementById("add-item"); 
 addBtn.addEventListener('click', addItem)
 
+const toDoList = document.getElementById("list")
+
+const textInput = document.getElementById("text-input")
 
 function addItem() {
-    alert(`We're working`)
+    // alert(`We're working`)
+    const newItem = document.createElement("p");
+    newItem.innerText = textInput.value;
+    toDoList.appendChild(newItem);
+    textInput.value = '';
+    
 }
